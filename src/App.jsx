f8bbd5f20b5ebda1c155e960e3771e53d2006e27
Dimas Rizky <dimasrizkym3@gmail.com>
2025-01-10 18:12:29 +0700

    fix: restrict access to login page for authenticated users

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ import ProtectedRoute from "./config/ProtectedRoute";
 import PageNotFound from "./pages/PageNotFound";
 import Index from "./pages/Index";
 import EditProfile from "./pages/EditProfile";
+import GuestRoute from "./config/GuestRoute";
 
 export default function App() {
   return (
     <div className="w-full min-h-screen bg-gray-100 dark:bg-gray-800 flex flex-col items-center">
       <Routes>
-        <Route path="login" element={<Login />} />
+        <Route
+          path="login"
+          element={
+            <GuestRoute>
+              <Login />
+            </GuestRoute>
+          }
+        />
         <Route path="*" element={<PageNotFound />} />
 
         <Route
